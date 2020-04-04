1bcb46601e0d66a2f7217bdb0c9426f7d5c8a5a5
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-04 16:16:42 -0300

    Update angular/core

---
+++ src/app/contador.action.ts
@@ -0,0 +1,5 @@
+import { createAction, props } from '@ngrx/store';
+
+export const inc = createAction('INCREMENTO');
+export const dec = createAction('DECREMENTO');
+export const random = createAction('RANDOM');
