1e89c6069c79bef48ee9c64e5ffc071b03224f7c
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2020-04-04 16:19:53 -0300

    Add actions

--- src/app/contador.action.ts
+++ src/app/contador.action.ts
@@ -3,3 +3,6 @@ import { createAction, props } from '@ngrx/store';
 export const inc = createAction('INCREMENTO');
 export const dec = createAction('DECREMENTO');
 export const random = createAction('RANDOM');
+export const saltar = createAction('SALTAR',
+  props<{ num: number }>()
+);
